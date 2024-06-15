7d2af38cb6c310d99c229440c2c731dbe8b06240
znjameswu <znjameswu@gmail.com>
2024-06-15 04:35:58 -0700

    [3/x] stash

--- epgi-2d/src/text.rs
+++ epgi-2d/src/text.rs
@@ -48,21 +48,13 @@ pub enum TextDecorationStyle {
 
 pub struct FontFeatures {}
 
-pub struct FontStyle{}
+pub struct FontStyle {}
 
-pub struct Paragraph {
-    text: Cow<'static, str>,
-    brush: ParleyBrush,
-    font_ctx: FontContext,
-}
-
-pub struct FontVariation {
-
-}
+pub struct FontVariation {}
 
 pub enum TextLeadingDistribution {
     Proportional,
-    Even
+    Even,
 }
 
 pub struct Locale {}
@@ -79,20 +71,7 @@ pub enum TextBaseline {
     Ideographic,
 }
 
-pub struct ParagraphLayout(pub(crate) parley::Layout<ParleyBrush>);
 
-impl ParagraphLayout {
-    pub fn size(&self) -> BoxSize {
-        BoxSize {
-            width: self.0.width(),
-            height: self.0.height(),
-        }
-    }
-}
-
-// For some reason, parley uses a RefCell in its FontContext
-// We have no other choice but to go for a mutex
-pub struct FontContext(Arc<SyncMutex<parley::FontContext>>);
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct ParleyBrush(pub vello::peniko::Brush);
@@ -108,35 +87,6 @@ impl Default for ParleyBrush {
 }
 
 impl parley::style::Brush for ParleyBrush {}
-
-impl Paragraph {
-    pub fn _new(
-        text: impl Into<Cow<'static, str>>,
-        brush: ParleyBrush,
-        font_ctx: FontContext,
-    ) -> Self {
-        Self {
-            text: text.into(),
-            brush,
-            font_ctx,
-        }
-    }
-
-    fn layout(&self, width: Option<f32>, alignment: TextAlign) -> ParagraphLayout {
-        let mut layout_ctx = parley::LayoutContext::new();
-        let mut font_ctx = self.font_ctx.0.lock();
-        let mut layout_builder = layout_ctx.ranged_builder(&mut font_ctx, &self.text, 1.0);
-        layout_builder.push_default(&parley::style::StyleProperty::Brush(self.brush.clone()));
-        // builder.push_default(&StyleProperty::FontSize(self.text_size));
-        // builder.push_default(&StyleProperty::FontStack(self.font));
-        // builder.push_default(&StyleProperty::FontWeight(self.weight));
-        // builder.push_default(&StyleProperty::FontStyle(self.style));
-        let mut layout = layout_builder.build();
-        drop(font_ctx);
-        layout.break_all_lines(width, alignment);
-        ParagraphLayout(layout)
-    }
-}
 
 // pub struct ParagraphBuilder {}
 
--- epgi-2d/src/text/paragraph.rs
+++ epgi-2d/src/text/paragraph.rs
@@ -1,6 +1,8 @@
 use std::{borrow::Cow, ops::Range};
 
-use crate::{LocalTextStyle, Paragraph, ParleyBrush};
+use epgi_core::foundation::{Arc, Asc, SyncMutex};
+
+use crate::{BoxSize, LocalTextStyle, ParleyBrush, TextAlign};
 
 pub struct ParagraphBuilder {
     text: Cow<'static, str>,
@@ -23,12 +25,55 @@ impl ParagraphBuilder {
 
     pub fn push(&mut self, text: impl Into<Cow<'static, str>>, style: Option<LocalTextStyle>) {
         self.text.to_mut().push_str(&text.into());
-        if let Some(style) = style {
-
-        }
+        if let Some(style) = style {}
     }
 
     pub fn build(self) -> Paragraph {
         todo!()
     }
 }
+
+pub struct Paragraph {
+    inner: Asc<ParagraphInner>,
+}
+
+struct ParagraphInner {
+    text: Cow<'static, str>,
+    styles: Vec<(
+        parley::style::StyleProperty<'static, ParleyBrush>,
+        Range<usize>,
+    )>,
+    font_ctx: FontContext,
+}
+
+impl Paragraph {
+    fn layout(&self, width: Option<f32>, alignment: TextAlign) -> ParagraphLayout {
+        let mut layout_ctx = parley::LayoutContext::new();
+        let mut font_ctx = self.inner.font_ctx.0.lock();
+        let mut layout_builder = layout_ctx.ranged_builder(&mut font_ctx, &self.inner.text, 1.0);
+        // layout_builder.push_default(&parley::style::StyleProperty::Brush(self.brush.clone()));
+        // builder.push_default(&StyleProperty::FontSize(self.text_size));
+        // builder.push_default(&StyleProperty::FontStack(self.font));
+        // builder.push_default(&StyleProperty::FontWeight(self.weight));
+        // builder.push_default(&StyleProperty::FontStyle(self.style));
+        let mut layout = layout_builder.build();
+        drop(font_ctx);
+        layout.break_all_lines(width, alignment);
+        ParagraphLayout(layout)
+    }
+}
+
+pub struct ParagraphLayout(pub(crate) parley::Layout<ParleyBrush>);
+
+impl ParagraphLayout {
+    pub fn size(&self) -> BoxSize {
+        BoxSize {
+            width: self.0.width(),
+            height: self.0.height(),
+        }
+    }
+}
+
+// For some reason, parley uses a RefCell in its FontContext
+// We have no other choice but to go for a mutex
+pub struct FontContext(Asc<SyncMutex<parley::FontContext>>);
