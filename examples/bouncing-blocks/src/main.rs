aad864f47789a0b5fb5ebf29dd149afb2165cd95
znjameswu <znjameswu@gmail.com>
2024-07-08 13:49:49 -0700

    update bouncing-block demo to fully utilize the functioning Provider

--- examples/bouncing-blocks/src/main.rs
+++ examples/bouncing-blocks/src/main.rs
@@ -2,9 +2,9 @@ use dpi::LogicalSize;
 use epgi_2d::{BoxConstraints, Color};
 use epgi_common::{
     AnimationControllerConf, AnimationFrame, BuildContextUseAnimationControllerExt, ColoredBox,
-    ConstrainedBox, GestureDetector, Positioned, Stack, ARC_PHANTOM_BOX,
+    ConstrainedBox, EdgeInsets, GestureDetector, Padding, Positioned, Stack, ARC_PHANTOM_BOX,
 };
-use epgi_core::{foundation::Asc, Builder, Consumer};
+use epgi_core::{foundation::Asc, Builder, Consumer, Provider};
 use epgi_material::MaterialApp;
 use epgi_winit::{AppLauncher, Window};
 use lazy_static::lazy_static;
@@ -63,121 +63,43 @@ lazy_static! {
 }
 
 fn main() {
-    let app = ConstrainedBox!(
-        constraints = BoxConstraints::new_tight(WIDTH, HEIGHT),
-        child = Consumer!(
-            builder = |ctx, animation_frame: Asc<AnimationFrame>| {
-                let (x, _animation_controller) = ctx.use_animation_controller_repeating_with(
-                    false,
-                    AnimationControllerConf!(duration = Duration::from_secs(DURATION_SECONDS)),
-                    Some(&animation_frame),
-                );
-                let time = x * DURATION_SECONDS as f32;
-                Stack!(
-                    children = BLOCK_DATA
-                        .iter()
-                        .map(|block_datum| {
-                            let effective_width = WIDTH - block_datum.r;
-                            let effective_height = HEIGHT - block_datum.r;
-                            let mut l = (block_datum.x + time * block_datum.vx)
-                                .rem_euclid(2.0 * effective_width);
-                            let mut t = (block_datum.y + time * block_datum.vy)
-                                .rem_euclid(2.0 * effective_height);
-
-                            if l > effective_width {
-                                l = 2.0 * effective_width - l;
-                            }
-
-                            if t > effective_height {
-                                t = 2.0 * effective_height - t;
-                            }
-                            Positioned!(
-                                l,
-                                t,
-                                child = Builder!(
-                                    builder = |ctx| {
-                                        let (clicked, set_clicked) = ctx.use_state(false);
-                                        let child = ctx.use_memo(
-                                            |clicked| {
-                                                ConstrainedBox!(
-                                                    constraints = BoxConstraints::new_tight(
-                                                        block_datum.r,
-                                                        block_datum.r
-                                                    ),
-                                                    child = GestureDetector!(
-                                                        on_tap = move |job_builder| {
-                                                            set_clicked.set(!clicked, job_builder);
-                                                        },
-                                                        child = ColoredBox!(
-                                                            color = if clicked {
-                                                                Color::rgba8(
-                                                                    0xFF,
-                                                                    0x98,
-                                                                    0x00,
-                                                                    block_datum.color.a,
-                                                                )
-                                                            } else {
-                                                                block_datum.color
-                                                            },
-                                                            child = ARC_PHANTOM_BOX.clone(),
-                                                        )
-                                                    )
-                                                )
-                                            },
-                                            clicked,
-                                        );
-                                        child
-                                    }
-                                )
-                            )
-                        })
-                        .collect()
-                )
-            }
-        )
-    );
-
+    // // This impl is faster on single thread but slower on multi-thread
     // let app = ConstrainedBox!(
     //     constraints = BoxConstraints::new_tight(WIDTH, HEIGHT),
-    //     child = Stack!(
-    //         children = BLOCK_DATA
-    //             .iter()
-    //             .map(|block_datum| {
-    //                 Positioned!(
-    //                     l = 0.0,
-    //                     t = 0.0,
-    //                     r = 0.0,
-    //                     b = 0.0,
-    //                     child = Consumer!(
-    //                         builder = |ctx, animation_frame: Asc<AnimationFrame>| {
-    //                             let (x, _animation_controller) = ctx
-    //                                 .use_animation_controller_repeating_with(
-    //                                     false,
-    //                                     AnimationControllerConf!(
-    //                                         duration = Duration::from_secs(DURATION_SECONDS)
-    //                                     ),
-    //                                     Some(&animation_frame),
-    //                                 );
-    //                             let time = x * DURATION_SECONDS as f32;
-    //                             let effective_width = WIDTH - block_datum.r;
-    //                             let effective_height = HEIGHT - block_datum.r;
-    //                             let mut l = (block_datum.x + time * block_datum.vx)
-    //                                 .rem_euclid(2.0 * effective_width);
-    //                             let mut t = (block_datum.y + time * block_datum.vy)
-    //                                 .rem_euclid(2.0 * effective_height);
+    //     child = Consumer!(
+    //         builder = |ctx, animation_frame: Asc<AnimationFrame>| {
+    //             let (x, _animation_controller) = ctx.use_animation_controller_repeating_with(
+    //                 false,
+    //                 AnimationControllerConf!(duration = Duration::from_secs(DURATION_SECONDS)),
+    //                 Some(&animation_frame),
+    //             );
+    //             let time = x * DURATION_SECONDS as f32;
+    //             Stack!(
+    //                 children = BLOCK_DATA
+    //                     .iter()
+    //                     .map(|block_datum| {
+    //                         let effective_width = WIDTH - block_datum.r;
+    //                         let effective_height = HEIGHT - block_datum.r;
+    //                         let mut l = (block_datum.x + time * block_datum.vx)
+    //                             .rem_euclid(2.0 * effective_width);
+    //                         let mut t = (block_datum.y + time * block_datum.vy)
+    //                             .rem_euclid(2.0 * effective_height);
 
-    //                             if l > effective_width {
-    //                                 l = 2.0 * effective_width - l;
-    //                             }
+    //                         if l > effective_width {
+    //                             l = 2.0 * effective_width - l;
+    //                         }
 
-    //                             if t > effective_height {
-    //                                 t = 2.0 * effective_height - t;
-    //                             }
-    //                             let child = ctx.use_memo(
-    //                                 |_| {
-    //                                     Builder!(
-    //                                         builder = |ctx| {
-    //                                             let (clicked, set_clicked) = ctx.use_state(false);
+    //                         if t > effective_height {
+    //                             t = 2.0 * effective_height - t;
+    //                         }
+    //                         Positioned!(
+    //                             l,
+    //                             t,
+    //                             child = Builder!(
+    //                                 builder = |ctx| {
+    //                                     let (clicked, set_clicked) = ctx.use_state(false);
+    //                                     let child = ctx.use_memo(
+    //                                         |clicked| {
     //                                             ConstrainedBox!(
     //                                                 constraints = BoxConstraints::new_tight(
     //                                                     block_datum.r,
@@ -202,26 +124,121 @@ fn main() {
     //                                                     )
     //                                                 )
     //                                             )
-    //                                         }
-    //                                     )
-    //                                 },
-    //                                 (),
-    //                             );
-    //                             Padding!(
-    //                                 padding = EdgeInsets::new()
-    //                                     .l(l)
-    //                                     .t(t)
-    //                                     .r(WIDTH - l - block_datum.r)
-    //                                     .b(HEIGHT - t - block_datum.r),
-    //                                 child
+    //                                         },
+    //                                         clicked,
+    //                                     );
+    //                                     child
+    //                                 }
     //                             )
-    //                         }
-    //                     )
-    //                 )
-    //             })
-    //             .collect()
+    //                         )
+    //                     })
+    //                     .collect()
+    //             )
+    //         }
     //     )
     // );
+
+    let app = Consumer!(
+        builder = |ctx, animation_frame: Asc<AnimationFrame>| {
+            let (x, _animation_controller) = ctx.use_animation_controller_repeating_with(
+                false,
+                AnimationControllerConf!(duration = Duration::from_secs(DURATION_SECONDS)),
+                Some(&animation_frame),
+            );
+            #[derive(PartialEq, Debug, Clone, Copy)]
+            struct AnimationTime {
+                value: f32,
+            }
+            let time = AnimationTime {
+                value: x * DURATION_SECONDS as f32,
+            };
+            let child = ctx.use_memo(
+                |_| {
+                    ConstrainedBox!(
+                        constraints = BoxConstraints::new_tight(WIDTH, HEIGHT),
+                        child = Stack!(
+                            children = BLOCK_DATA
+                                .iter()
+                                .map(|block_datum| {
+                                    Positioned!(
+                                        l = 0.0,
+                                        t = 0.0,
+                                        r = 0.0,
+                                        b = 0.0,
+                                        child = Consumer!(
+                                            builder = |ctx, time: Asc<AnimationTime>| {
+                                                let effective_width = WIDTH - block_datum.r;
+                                                let effective_height = HEIGHT - block_datum.r;
+                                                let mut l = (block_datum.x + time.value * block_datum.vx)
+                                                    .rem_euclid(2.0 * effective_width);
+                                                let mut t = (block_datum.y + time.value * block_datum.vy)
+                                                    .rem_euclid(2.0 * effective_height);
+
+                                                if l > effective_width {
+                                                    l = 2.0 * effective_width - l;
+                                                }
+
+                                                if t > effective_height {
+                                                    t = 2.0 * effective_height - t;
+                                                }
+                                                let child = ctx.use_memo(
+                                                    |_| {
+                                                        Builder!(
+                                                            builder =
+                                                                |ctx| {
+                                                                    let (clicked, set_clicked) =
+                                                                        ctx.use_state(false);
+                                                                    ConstrainedBox!(
+                                                            constraints = BoxConstraints::new_tight(
+                                                                block_datum.r,
+                                                                block_datum.r
+                                                            ),
+                                                            child = GestureDetector!(
+                                                                on_tap = move |job_builder| {
+                                                                    set_clicked
+                                                                        .set(!clicked, job_builder);
+                                                                },
+                                                                child = ColoredBox!(
+                                                                    color = if clicked {
+                                                                        Color::rgba8(
+                                                                            0xFF,
+                                                                            0x98,
+                                                                            0x00,
+                                                                            block_datum.color.a,
+                                                                        )
+                                                                    } else {
+                                                                        block_datum.color
+                                                                    },
+                                                                    child = ARC_PHANTOM_BOX.clone(),
+                                                                )
+                                                            )
+                                                        )
+                                                                }
+                                                        )
+                                                    },
+                                                    (),
+                                                );
+                                                Padding!(
+                                                    padding = EdgeInsets::new()
+                                                        .l(l)
+                                                        .t(t)
+                                                        .r(WIDTH - l - block_datum.r)
+                                                        .b(HEIGHT - t - block_datum.r),
+                                                    child
+                                                )
+                                            }
+                                        )
+                                    )
+                                })
+                                .collect()
+                        )
+                    )
+                },
+                (),
+            );
+            Provider!(value = time, child)
+        }
+    );
 
     let app = MaterialApp!(child = app);
 
